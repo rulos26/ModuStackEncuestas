1473f2eeeb20a6428ab6c0485a4172ce584080ee
juan.diaz <rulos26@gmail.com>
2025-07-12 13:32:01 -0500

    Actualizar la plantilla 'layouts.app' para cargar los archivos CSS y JS de forma directa, reemplazando la configuración de Vite, lo que mejora la gestión de recursos en la aplicación.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,8 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('public/build/assets/app-DbaZCfaT.css') }}">
+    <script type="module" src="{{ asset('public/build/assets/app-BDAque31.js') }}"></script>
 </head>
 <body>
     <div id="app">
