a25e57de0b34993fa49d8df8b5d6017d382f0702
juan.diaz <rulos26@gmail.com>
2025-07-15 19:54:22 -0500

    Actualizar la configuración de 'adminlte.php' para establecer el modo oscuro en 'auto' y traducir términos en 'adminlte.php' al español, mejorando la accesibilidad y la experiencia del usuario en la interfaz.

--- config/adminlte.php
+++ config/adminlte.php
@@ -157,7 +157,7 @@ return [
     'layout_fixed_sidebar' => null,
     'layout_fixed_navbar' => null,
     'layout_fixed_footer' => null,
-    'layout_dark_mode' => null,
+    'layout_dark_mode' => 'auto',
 
     /*
     |--------------------------------------------------------------------------
