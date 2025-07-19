a64ce098bad056a82a52a055c0e21b06a07e7891
juan.diaz <rulos26@gmail.com>
2025-07-19 11:05:03 -0500

    Agregar mensaje de validación para archivos de tipo en español en el archivo de validaciones. Se incluye la validación para asegurar que el archivo subido sea del tipo permitido.

--- resources/lang/es/validation.php
+++ resources/lang/es/validation.php
@@ -3,5 +3,6 @@
 return [
     'digits' => 'El campo :attribute debe tener exactamente :digits dígitos.',
     'regex' => 'El formato del campo :attribute no es válido.',
+    'mimes' => 'El archivo :attribute debe ser un archivo de tipo: :values.',
     // Puedes agregar más mensajes personalizados aquí si lo deseas
 ];
