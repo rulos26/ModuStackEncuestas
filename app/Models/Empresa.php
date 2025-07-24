f3359a5bcbcf9832352c3a6df3a0b50f1adac7d0
rulos26 <rulos26@gmail.com>
2025-07-24 10:27:27 -0500

    Corregir nombre de la tabla en el modelo Empresa de 'empresa' a 'empresas'

--- app/Models/Empresa.php
+++ app/Models/Empresa.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Empresa extends Model
 {
-    protected $table = 'empresa';
+    protected $table = 'empresas';
     protected $fillable = [
         'nombre_legal', 'nit', 'representante_legal', 'telefono', 'email', 'direccion',
         'pais_id', 'departamento_id', 'municipio_id', 'mision', 'vision', 'descripcion', 'fecha_creacion'
