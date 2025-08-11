20f15a4ac4f97e2e44df82b4dc5a5d775eb36442
rulos26 <rulos26@gmail.com>
2025-08-11 08:41:25 -0500

    Cambiar la extensión de las vistas de confirmación de eliminación a 'adminlte::page' para unificar el diseño y mejorar la consistencia visual en la gestión de encuestas.

--- resources/views/encuestas/confirmar-eliminacion-masiva.blade.php
+++ resources/views/encuestas/confirmar-eliminacion-masiva.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('adminlte::page')
 
 @section('title', 'Confirmar Eliminación Masiva')
 
--- resources/views/encuestas/eliminacion-masiva.blade.php
+++ resources/views/encuestas/eliminacion-masiva.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('adminlte::page')
 
 @section('title', 'Eliminación Masiva de Encuestas')
 
