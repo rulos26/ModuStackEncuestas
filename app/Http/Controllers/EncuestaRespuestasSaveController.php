c1def58c372608c9b395aac20db4fc43964d344b
rulos26 <rulos26@gmail.com>
2025-08-15 09:32:55 -0500

    Incluir la búsqueda de la encuesta en el método 'save' de EncuestaRespuestasSaveController, añadiendo el objeto de encuesta al volcado de datos para mejorar la trazabilidad y depuración de respuestas.

--- app/Http/Controllers/EncuestaRespuestasSaveController.php
+++ app/Http/Controllers/EncuestaRespuestasSaveController.php
@@ -11,11 +11,14 @@ class EncuestaRespuestasSaveController extends Controller
     public function save(Request $request, $id)
     {
 
+        $encuesta = Encuesta::with(['preguntas.respuestas'])->where('id',$id)->firstOrFail();
+
         dd( 'encuesta_id' ,$id,
         'request_data' , $request->all(),
         'request_url' , request()->fullUrl(),
         'user_agent' , request()->userAgent(),
         'ip' , request()->ip(),
-        'timestamp' , now()->toDateTimeString());
+        'timestamp' , now()->toDateTimeString(),
+        'encuesta' , $encuesta);
     }
 }
